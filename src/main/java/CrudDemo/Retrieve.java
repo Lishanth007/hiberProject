605a37eff9c63eccfb8041d040b9a93d064e58e1
ASUS <ASUS@LAPTOP-EUT7R324>
2023-11-03 16:56:15 +0530

    no

--- src/main/java/CrudDemo/Retrieve.java
+++ src/main/java/CrudDemo/Retrieve.java
@@ -1,10 +1,25 @@
 package CrudDemo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.engine.jdbc.env.internal.DefaultSchemaNameResolver.SchemaNameResolverJava17Delegate;
+
 public class Retrieve {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		public static void main(String[] args) {
+			SessionFactory sf = hiberConfig.ImplementSessionFactroy();
+			
+			Session sObj = sf.openSession();
+			
+			Transaction ts = sObj.beginTransaction();
+			
+			Aliens a1 = sObj.get(Aliens.class, 1);
+			System.out.println("AlienID: "+a1.getAid());
+			System.out.println("AlienID: "+a1.getAlienname());
+			System.out.println("AlienID: "+a1.getColor());
+			System.out.println("AlienID: "+a1.getPlante());
 	}
 
 }
